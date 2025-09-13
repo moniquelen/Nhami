bd5554f4d95f3a073d9f24d56cbc2d494e4985a2
Monique Elen <moniqueelen@id.uff.br>
2025-09-13 14:15:19 -0300

    melhorias gerais

--- scripts.js
+++ scripts.js
@@ -1,89 +1,36 @@
 var products = [
-        {
-            "photo": "img/tacos.png",
-            "name": "Tacos",
-            "price": 16.99,
-            "active": false,
-            "quantity": 1
-        },
-        {
-            "photo": "img/hotdog.png",
-            "name": "Cachorro Quente",
-            "price": 15.99,
-            "active": false,
-            "quantity": 1
-        },
-        {
-            "photo": "img/sandwich.png",
-            "name": "Sanduíche",
-            "price": 9.99,
-            "active": false,
-            "quantity": 1
-        },
-        {
-            "photo": "img/chocolate-cake.png",
-            "name": "Bolo de Chocolate",
-            "price": 7.99,
-            "active": false,
-            "quantity": 1
-        },
-        {
-            "photo": "img/strawberry-cake.png",
-            "name": "Bolo de Morango",
-            "price": 7.99,
-            "active": false,
-            "quantity": 1
-        },
-        {
-            "photo": "img/chocolate-pie.png",
-            "name": "Torta de Chocolate",
-            "price": 9.99,
-            "active": false,
-            "quantity": 1
-        },
-        {
-            "photo": "img/bubble-tea.png",
-            "name": "Bubble Tea",
-            "price": 12.99,
-            "active": false,
-            "quantity": 1
-        },
-        {
-            "photo": "img/soda.png",
-            "name": "Refrigerante",
-            "price": 7.99,
-            "active": false,
-            "quantity": 1
-        },
-        {
-            "photo": "img/coffee.png",
-            "name": "Café",
-            "price": 14.99,
-            "active": false,
-            "quantity": 1
-        }
+    { "photo": "media/img/products/tacos.png", "name": "Tacos", "price": 16.99, "active": false, "quantity": 1 },
+    { "photo": "media/img/products/hotdog.png", "name": "Cachorro Quente", "price": 15.99, "active": false, "quantity": 1 },
+    { "photo": "media/img/products/sandwich.png", "name": "Sanduíche", "price": 9.99, "active": false, "quantity": 1 },
+    { "photo": "media/img/products/chocolate-cake.png", "name": "Bolo de Chocolate", "price": 7.99, "active": false, "quantity": 1 },
+    { "photo": "media/img/products/strawberry-cake.png", "name": "Bolo de Morango", "price": 7.99, "active": false, "quantity": 1 },
+    { "photo": "media/img/products/chocolate-pie.png", "name": "Torta de Chocolate", "price": 9.99, "active": false, "quantity": 1 },
+    { "photo": "media/img/products/bubble-tea.png", "name": "Bubble Tea", "price": 12.99, "active": false, "quantity": 1 },
+    { "photo": "media/img/products/soda.png", "name": "Refrigerante", "price": 7.99, "active": false, "quantity": 1 },
+    { "photo": "media/img/products/coffee.png", "name": "Café", "price": 14.99, "active": false, "quantity": 1 }
 ];
 
 const SelfServiceMachine = {
     data() {
         return {
-            products: window.products,
-            orderTotal: 0,
+            products: products.map(p => ({ ...p })),
+            showSummary: false,
             showPopup: false
         }
     },
     methods: {
-        toggleActive: function(item){
+        toggleActive(item) {
             item.active = !item.active;
+            this.showSummary = this.products.some(p => p.active);
         },
-        total: function(){
-            var total = 0;
-            this.products.forEach(function(item){
-                if (item.active){
-                    total+= item.price * item.quantity;
+        total() {
+            let total = 0;
+            this.products.forEach(item => {
+                if (item.active) {
+                    total += item.price * item.quantity;
                 }
             });
-           return total.toFixed(2);
+            return total.toFixed(2);
         },
         finishOrder() {
             const selectedProducts = this.products.filter(p => p.active);
@@ -95,15 +42,19 @@ const SelfServiceMachine = {
                 })),
                 total: this.total()
             };
-            
+
             localStorage.setItem('lastOrder', JSON.stringify(order));
             
             this.showPopup = true;
-
+        },
+        closePopup() {
+            this.showPopup = false;
             this.products.forEach(product => {
                 product.active = false;
                 product.quantity = 1;
             });
+
+            this.showSummary = false;
         }
     }
 };
